93c74919616c9e47f9c5adfd7f85347391da9e0f
Silver0x10 <smanganar@gmail.com>
2023-03-23 12:34:07 +0100

    quaternion2RPY in the main

--- executables/structure_from_motion.cpp
+++ executables/structure_from_motion.cpp
@@ -26,24 +26,12 @@ int main (int argc, char** argv) {
             for(int i=0; i<3; ++i) line_stream >> position[i];
             for(int i=0; i<3; ++i) line_stream >> orientation[i];
             
-            // quaternion_to_angle HERE
+            quaternion_to_RPY(gt_orientation);
+            quaternion_to_RPY(orientation);
 
             Camera cam(id, gt_position, gt_orientation, position, orientation);
             cameras.push_back(cam);
             current_cam = &cameras.back();
-
-            // TODO: Remove
-            // cout << "KF " << id;
-            // cout << "Camera id: " << cam.id << endl;
-            // cout << " gt pos: ";
-            // for(int i=0; i<3; ++i) cout << " " << cam.gt_position(i, 0);
-            // cout << " gt or: ";
-            // for(int i=0; i<3; ++i) cout << " " << cam.gt_orientation(i, 0);
-            // cout << " pos: ";
-            // for(int i=0; i<3; ++i) cout << " " << cam.position(i, 0);
-            // cout << " or: ";
-            // for(int i=0; i<3; ++i) cout << " " << cam.orientation(i, 0);
-            // cout << endl;
 
         }
         else if(!line_type.compare("F:")) { // Keypoint
@@ -55,23 +43,13 @@ int main (int argc, char** argv) {
 
             Keypoint kp(id, direction_vector);
             current_cam->keypoints.push_back(kp);
-
-            // cout << "F " << prog_id << " " << kp.id;
-            // for(int i=0; i<3; ++i) cout << " " << kp.direction_vector(i, 0);
-            // cout << endl;
         }
-        // delete current_cam;
-
-
-        // break;
+        
     }
 
-    // for(int i=0; i<cameras.size(); ++i ){
-    //     auto cam = cameras[i];
-    //     cout << cam.id << " kps: " << cam.keypoints.size() << endl;
-    // }
-    auto test = cameras[5].keypoints[5];
-    cout << test.id << endl;
-    cout << test.direction_vector << endl;
+    
+    // auto test = cameras[5].keypoints[5];
+    // cout << test.id << endl;
+    // cout << test.direction_vector << endl;
 
 }
