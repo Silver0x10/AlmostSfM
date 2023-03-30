b73c0eb7ba04a978d5aaad25ff929a3bab6c53c2
Silver0x10 <smanganar@gmail.com>
2023-03-30 13:34:09 +0200

    init_translations state and H matrix definition

--- src/init_translations.cpp
+++ src/init_translations.cpp
@@ -4,9 +4,24 @@
 
 namespace pr {
 
-    void init_translations(vector<Camera>& cameras) {
-        Eigen::Matrix<float, 3, 3> H;
-        cout << H;
+    void error_and_jacobian_translation(const Vec3f& t_ij, const Matrix3f& rot_i, const Vec3f& t_i, const Vec3f& t_j, Vec3f& error, Eigen::MatrixXf& jacobian) {
     }
+
+    void init_translations(vector<Camera>& cameras) {
+        int system_size = pow(cameras.size(), 2);
+        Vec3f state[system_size];
+        for(auto t: state) t.setZero();
+        
+        Eigen::MatrixXf H(3*system_size, 3*system_size);
+        Eigen::VectorXf b(3*system_size);
+        
+        // chi errror
+
+        H.setZero();
+        b.setZero();
+        
+
+    }
+
 
 }
--- src/init_translations.h
+++ src/init_translations.h
@@ -2,9 +2,9 @@
 #include <Eigen/Core>
 #include "utils.h"
 
-using namespace std;
-
 namespace pr {
+
+    void error_and_jacobian_translation(const Vec3f& t_ij, const Matrix3f& R_i, const Vec3f& t_i, const Vec3f& t_j, Vec3f& error, Eigen::MatrixXf& jacobian);
 
     void init_translations(vector<Camera>& cameras);
 
