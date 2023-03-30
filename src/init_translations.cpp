882c27a91e7f0f43356ce97977c4bcfa40dce6d8
Silver0x10 <smanganar@gmail.com>
2023-03-30 19:08:36 +0200

    first rude sketch of the LS for translations; not working, too slow. it uses the full jacobian

--- src/init_translations.cpp
+++ src/init_translations.cpp
@@ -1,3 +1,4 @@
+// #pragma once
 #include "init_translations.h"
 
 namespace pr {
@@ -19,10 +20,12 @@ namespace pr {
         jacobian.block<3,3>(0, 3*(i*num_cameras + j)) = jac_t_ij;
     }
 
+
     void init_translations(vector<Camera>& cameras) {
-        int system_size = pow(cameras.size(), 2);
+        int num_cameras = cameras.size();
+        int system_size = pow(num_cameras, 2);
         Vec3f state[system_size];
-        for(auto t: state) t.setZero();
+        for(auto& t: state) t.setRandom(); // TODO: maybe consider damping factors instead 
         
         Eigen::MatrixXf H(3*system_size, 3*system_size);
         Eigen::VectorXf b(3*system_size);
@@ -32,6 +35,28 @@ namespace pr {
         H.setZero();
         b.setZero();
         
+
+        for(int i=0; i<num_cameras; ++i){
+            for(int j=0; j<num_cameras; ++j){
+                const auto& t_ij = state[i*num_cameras + j];
+                const auto& rot_i = v2tRPY(cameras[i].orientation);
+                const auto& t_i = state[i*num_cameras];
+                const auto& t_j = state[j*num_cameras];
+                
+                Vec3f error;
+                Eigen::MatrixXf jacobian(3, 3*system_size); // TODO: use only the modified elments instead of the entire matrix
+                error_and_jacobian_translation(t_ij, rot_i, t_i, t_j, error, jacobian, num_cameras, i, j);
+
+                // H += jacobian.transpose()*jacobian;
+                // b += jacobian.transpose()*error;
+
+                cout << i << ' ' << j << endl;
+            }
+        }
+
+        // auto dx = H.ldlt().solve(-b); // TODO: ripasso Cholesky decomposition; consider the null space of H
+
+
 
     }
 
