9eeea2db7ca8563841c64ea88ef4fe9b9841e666
Silver0x10 <smanganar@gmail.com>
2023-05-23 17:35:40 +0200

    3d map visualization (using opencv::viz)

--- executables/structure_from_motion.cpp
+++ executables/structure_from_motion.cpp
@@ -1,13 +1,27 @@
 #include <iostream>
 #include <string>
 #include <vector>
+#include <map>
 #include "utils.h"
 #include "init_translations.h"
 #include "relative_position_calculator.h"
 #include "triangulation.h"
+#include <opencv2/viz.hpp>
 
 using namespace std;
 using namespace pr;
+
+
+void map_visualization(map<int, pr::Vec3f> estimated_landmarks){
+    cv::viz::Viz3d window("Map visualization");
+    std::vector<cv::Point3d> landmarks_cv;
+    for(const auto& l: estimated_landmarks) 
+        landmarks_cv.push_back( cv::Point3d(l.second.x(), l.second.y(), l.second.z()) );
+    cv::viz::WCloud landmarks_cloud(landmarks_cv, cv::viz::Color::green());
+    landmarks_cloud.setRenderingProperty( cv::viz::POINT_SIZE, 1.5 );
+    window.showWidget("landmarks", landmarks_cloud);
+    window.spin();
+}
 
 int main (int argc, char** argv) {
     string dataset_path = "../../dataset_and_info/dataset.txt";
@@ -16,7 +30,9 @@ int main (int argc, char** argv) {
 
     init_translations(cameras);
     // eval_translations(cameras);
-    triangulate(cameras);
+    auto landmarks = triangulate(cameras);
+    map_visualization(landmarks);
+    
 
     return 0;
 
