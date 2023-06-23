983f180644ec149af2eba25f5481e5ea13bdfcc3
Silver0x10 <smanganar@gmail.com>
2023-06-23 16:52:38 +0200

    visualization all at once

--- executables/structure_from_motion.cpp
+++ executables/structure_from_motion.cpp
@@ -9,11 +9,12 @@
 #include "relative_position_calculator.h"
 #include "triangulation.h"
 #include "bundle_adjustment.h"
+#include "evaluation.h"
 
 using namespace std;
 using namespace pr;
 
-void map_visualization(string landmarks_path,  string gt_landmarks_path){
+void visualize(const vector<Camera>& cameras, string landmarks_path,  string gt_landmarks_path){
     cv::viz::Viz3d window("Map visualization");
 
     // Estimated Landmarks visualization
@@ -22,8 +23,15 @@ void map_visualization(string landmarks_path,  string gt_landmarks_path){
     for(const auto& l: landmarks) 
         landmarks_cv.push_back( cv::Point3d(l.second.x(), l.second.y(), l.second.z()) );
     cv::viz::WCloud landmarks_cloud(landmarks_cv, cv::viz::Color::red());
-    landmarks_cloud.setRenderingProperty( cv::viz::POINT_SIZE, 2 );
+    landmarks_cloud.setRenderingProperty( cv::viz::POINT_SIZE, 3 );
     window.showWidget("landmarks", landmarks_cloud);
+
+    std::vector<cv::Point3d> cameras_cv;
+    for(const auto& c: cameras) 
+        cameras_cv.push_back( cv::Point3d( c.position.x(), c.position.y(), c.position.z()) );
+    cv::viz::WCloud cameras_cloud(cameras_cv, cv::viz::Color::blue());
+    cameras_cloud.setRenderingProperty( cv::viz::POINT_SIZE, 3 );
+    window.showWidget("cameras", cameras_cloud);
 
     // GT Landmarks visualization
     map<int, pr::Vec3f> gt_landmarks = load_landmarks(gt_landmarks_path);
@@ -31,8 +39,16 @@ void map_visualization(string landmarks_path,  string gt_landmarks_path){
     for(const auto& l: gt_landmarks) 
         gt_landmarks_cv.push_back( cv::Point3d(l.second.x(), l.second.y(), l.second.z()) );
     cv::viz::WCloud gt_landmarks_cloud(gt_landmarks_cv, cv::viz::Color::green());
-    gt_landmarks_cloud.setRenderingProperty( cv::viz::POINT_SIZE, 2 );
+    gt_landmarks_cloud.setRenderingProperty( cv::viz::POINT_SIZE, 3 );
     window.showWidget("gt_landmarks", gt_landmarks_cloud);
+
+    // GT Cameras visualization
+    std::vector<cv::Point3d> gt_cameras_cv;
+    for(const auto& c: cameras) 
+        gt_cameras_cv.push_back( cv::Point3d( c.gt_position.x(), c.gt_position.y(), c.gt_position.z()) );
+    cv::viz::WCloud gt_cameras_cloud(gt_cameras_cv, cv::viz::Color::violet());
+    gt_cameras_cloud.setRenderingProperty( cv::viz::POINT_SIZE, 3 );
+    window.showWidget("gt_cameras", gt_cameras_cloud);
 
     window.spin();
 }
@@ -42,20 +58,21 @@ int main (int argc, char** argv) {
     string out_camera_positions = argv[2];
     string out_landmark_positions = argv[3];
     string gt_landmark_positions = argv[4];
+    string output_dir = out_landmark_positions.substr(0, out_camera_positions.rfind('/'));
 
     vector<Camera> cameras = load_data(dataset_path);
     cout << "Data loaded from: " << dataset_path << endl << endl;
 
-    cout << "Initialization...";
-    init_translations(cameras);
-    cout << "\tDONE" << endl;
+    // cout << "Initialization...";
+    // init_translations(cameras);
+    // cout << "\tDONE" << endl;
     
     cout << "Triangulation... ";
     auto landmarks = triangulate(cameras);
     cout << "\tDONE" << endl;
     
     cout << endl << "Bundle Adjustment... " << endl;
-    bundle_adjustment(cameras, landmarks);
+    // bundle_adjustment(cameras, landmarks);
     cout << "DONE" << endl << endl;
 
     save_camera_positions(cameras, out_camera_positions);
@@ -64,12 +81,13 @@ int main (int argc, char** argv) {
     save_landmarks(landmarks, out_landmark_positions);
     cout << "Landmark positions saved in: " << out_landmark_positions << endl << endl;
 
-    // cout << "Evaluation... ";
-    // map<int, pr::Vec3f> gt_landmarks = load_landmarks(gt_landmark_positions);
-    // evaluation(cameras, landmarks, gt_landmarks);
-    // cout << "\tDONE" << endl;
+    map<int, pr::Vec3f> gt_landmarks = load_landmarks(gt_landmark_positions);
 
-    map_visualization(out_landmark_positions, gt_landmark_positions);
+    cout << "Evaluation... ";
+    evaluation(cameras, landmarks, gt_landmarks, output_dir);
+    cout << "\tDONE" << endl;
+
+    visualize(cameras, out_landmark_positions, gt_landmark_positions);
 
     return 0;
 
