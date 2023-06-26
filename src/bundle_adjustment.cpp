dec7353a62a24d5515c6b6d986dd6b0e543badc6
Silver0x10 <smanganar@gmail.com>
2023-06-26 20:06:10 +0200

    BA using epipolar constaint as Pose-Pose error and cross product between true and estimated direction vector as Pose-Landmark error

--- src/bundle_adjustment.cpp
+++ src/bundle_adjustment.cpp
@@ -2,121 +2,168 @@
 
 namespace pr {
 
-    void error_and_jacobian_camera_poses(const Vec3f& t_ij, const Matrix3f& rot_i, const Vec3f& t_i, const Vec3f& t_j, Vec3f& error, Eigen::MatrixXf& jacobians) {
-        error = skew(t_ij) * rot_i.transpose() * (t_i - t_j);
+    void error_and_jacobian_camera_poses(const Vec3f& t_ij, const Vec3f& rot_angles_i, const Vec3f& t_i, const Vec3f& t_j, Vec3f& error, Eigen::MatrixXf& jacobians) {
+        auto rot_i = v2tRPY(rot_angles_i);
+        error = skew(t_ij) * rot_i.transpose() * (t_j - t_i);
 
-        Matrix3f jac_t_i = skew(t_ij) * rot_i.transpose();
-        Matrix3f jac_t_j = -jac_t_i;
-        
-        jacobians.block<3,3>(0, 0) = jac_t_i;
-        jacobians.block<3,3>(0, 3) = jac_t_j;
+        jacobians.block<3,3>(0, 0) = -skew(t_ij) * rot_i.transpose(); // wrt the camera i position
+
+        jacobians.block<3,3>(0, 3) = skew(t_ij) * rot_i.transpose(); // wrt the camera j position
+
+        jacobians.block<3,1>(0, 6) = skew(t_ij) * (Rx_prime(rot_angles_i[0])*Ry(rot_angles_i[1])*Rz(rot_angles_i[2])).transpose() * (t_i - t_j); // wrt the camera orientation wrt x
+        jacobians.block<3,1>(0, 7) = skew(t_ij) * (Rx(rot_angles_i[0])*Ry_prime(rot_angles_i[1])*Rz(rot_angles_i[2])).transpose() * (t_i - t_j); // wrt the camera orientation wrt y
+        jacobians.block<3,1>(0, 8) = skew(t_ij) * (Rx(rot_angles_i[0])*Ry(rot_angles_i[1])*Rz_prime(rot_angles_i[2])).transpose() * (t_i - t_j); // wrt the camera orientation wrt z
     }
 
     void error_and_jacobian_position_landmark(const Camera& camera, const Vec3f& keypoint_dir_vector, const Vec3f& landmark, Vec3f& error, Eigen::MatrixXf& jacobians) {
-        auto delta_position = landmark - camera.position;
-        error = skew(keypoint_dir_vector) * delta_position;
+        auto dir_vector_hat = v2tRPY(camera.orientation).transpose()*(landmark - camera.position);
+        // dir_vector_hat.normalize();
+        error = skew(keypoint_dir_vector) * dir_vector_hat;
         
-        jacobians.block<3,3>(0, 0) = -skew(keypoint_dir_vector); // wrt the camera
-        jacobians.block<3,3>(0, 3) = skew(keypoint_dir_vector);  // wrt the landmark
+        auto alpha = camera.orientation[0];
+        auto beta = camera.orientation[1];
+        auto gamma = camera.orientation[2];
+        jacobians.block<3,1>(0, 0) = skew(keypoint_dir_vector) * (Rx_prime(alpha)*Ry(beta)*Rz(gamma)).transpose() * (landmark - camera.position); // wrt the camera orientation wrt x
+        jacobians.block<3,1>(0, 1) = skew(keypoint_dir_vector) * (Rx(alpha)*Ry_prime(beta)*Rz(gamma)).transpose() * (landmark - camera.position); // wrt the camera orientation wrt y
+        jacobians.block<3,1>(0, 2) = skew(keypoint_dir_vector) * (Rx(alpha)*Ry(beta)*Rz_prime(gamma)).transpose() * (landmark - camera.position); // wrt the camera orientation wrt z
+
+        jacobians.block<3,3>(0, 3) = -skew(keypoint_dir_vector) * v2tRPY(camera.orientation).transpose(); // wrt the camera position
+
+        jacobians.block<3,3>(0, 6) = skew(keypoint_dir_vector) * v2tRPY(camera.orientation).transpose();  // wrt the landmark
     }
     
-    void bundle_adjustment(vector<Camera>& cameras, map<int, Vec3f>& landmarks){
-        int system_size = cameras.size() + landmarks.size();
+    void bundle_adjustment(vector<Camera>& cameras, map<int, Vec3f>& landmarks, int rounds){
+        int system_size = 2*cameras.size() + landmarks.size();
         
         // (Like) Data association
         vector<int> landmark_index2id;
         for(const auto& l: landmarks) landmark_index2id.push_back(l.first);
         map<int, int> landmark_id2index;
         for(int i=0; i<(int)landmark_index2id.size(); i++) landmark_id2index.insert({landmark_index2id[i], i});
+        // for(const auto& l: landmarks) { // just for checking if the association is correct
+        //     int index = landmark_id2index[l.first];
+        //     cout << l.first << "\t" << index << "\t" << landmark_index2id[index] << endl;
+        // }
 
         Eigen::MatrixXf matrix_H(3*system_size, 3*system_size);
         Eigen::MatrixXf b(3*system_size, 1);
 
-        for (int iteration = 0; iteration < 5; iteration++) {
+        for (int iteration = 0; iteration < rounds; iteration++) {
             cout << "BA iteration: " << iteration << endl;
             matrix_H.setZero();
             b.setZero();
             float chi_tot = 0.0;
 
             for(const auto& cam: cameras) {
-
+                
+                // Pose-Landmark
                 for(const auto& kp: cam.keypoints) {
                     Vec3f error;
-                    Eigen::MatrixXf jacobians(3, 6);
+                    Eigen::MatrixXf jacobians(3, 9);
                     Vec3f l = landmarks[kp.id];
                     error_and_jacobian_position_landmark(cam, kp.direction_vector, l, error, jacobians);
                     float chi = error.transpose() * error;
-                    // if(chi > 1) cout << chi << endl;
-                    if(chi > 1) continue;
-
                     chi_tot += chi;
-                    Matrix3f jac_cam = (Matrix3f)(jacobians.block<3,3>(0, 0));
-                    Matrix3f jac_landmark = (Matrix3f)(jacobians.block<3,3>(0, 3));
 
-                    int i = 3*cam.id;
-                    int j = 3*(cameras.size()-1 + landmark_id2index[kp.id]);
-                    matrix_H.block<3,3>(i, i) += jac_cam.transpose()*jac_cam;
-                    matrix_H.block<3,3>(i, j) += jac_cam.transpose()*jac_landmark;
-                    matrix_H.block<3,3>(j, i) += jac_landmark.transpose()*jac_cam;
-                    matrix_H.block<3,3>(j, j) += jac_landmark.transpose()*jac_landmark; 
-                    b.block<3,1>(i, 0) += jac_cam.transpose()*error;
+                    Matrix3f jac_cam_orientation = (Matrix3f)(jacobians.block<3,3>(0, 0));
+                    Matrix3f jac_cam_position = (Matrix3f)(jacobians.block<3,3>(0, 3));
+                    Matrix3f jac_landmark = (Matrix3f)(jacobians.block<3,3>(0, 6));
+
+                    int i_position = 3*cam.id;
+                    int i_orientation = i_position + 3*cameras.size();
+                    int j = 3*( 2*cameras.size() + landmark_id2index[kp.id] );
+
+                    matrix_H.block<3,3>(i_position, i_position)    += jac_cam_position.transpose()*jac_cam_position;
+                    matrix_H.block<3,3>(i_position, i_orientation) += jac_cam_position.transpose()*jac_cam_orientation;
+                    matrix_H.block<3,3>(i_position, j)             += jac_cam_position.transpose()*jac_landmark;
+
+                    matrix_H.block<3,3>(i_orientation, i_position)    += jac_cam_orientation.transpose()*jac_cam_position;
+                    matrix_H.block<3,3>(i_orientation, i_orientation) += jac_cam_orientation.transpose()*jac_cam_orientation;
+                    matrix_H.block<3,3>(i_orientation, j)             += jac_cam_orientation.transpose()*jac_landmark;
+                    
+                    matrix_H.block<3,3>(j, i_position)    += jac_landmark.transpose()*jac_cam_position;
+                    matrix_H.block<3,3>(j, i_orientation) += jac_landmark.transpose()*jac_cam_orientation;
+                    matrix_H.block<3,3>(j, j)             += jac_landmark.transpose()*jac_landmark;
+                    
+                    b.block<3,1>(i_position, 0) += jac_cam_position.transpose()*error;
+                    b.block<3,1>(i_orientation, 0) += jac_cam_orientation.transpose()*error;
                     b.block<3,1>(j, 0) += jac_landmark.transpose()*error;
                 }
-
-                for(const auto& cam2: cameras){
+                
+                // Pose-Pose
+                for(const auto& cam2: cameras) {
                     if(cam2.id == cam.id) continue;
-                    // as in init translations
-                    const auto& t_ij = calculate_relative_position(cam, cam2);
-                    const auto& rot_i = v2tRPY(cam.orientation);
+                    const auto& t_ij = v2tRPY(cam.gt_orientation).transpose()*(cam2.gt_position - cam.gt_position); // GT relative position for checking correctness
+                    // const auto& t_ij = calculate_relative_position(cam, cam2);
+                    const auto& rot_angles_i = cam.orientation;
                     const auto& t_i = cam.position;
                     const auto& t_j = cam2.position;
                     
                     Vec3f error;
-                    Eigen::MatrixXf jacobians(3, 6);
-                    error_and_jacobian_camera_poses(t_ij, rot_i, t_i, t_j, error, jacobians);
+                    Eigen::MatrixXf jacobians(3, 9);
+                    error_and_jacobian_camera_poses(t_ij, rot_angles_i, t_i, t_j, error, jacobians);
                     float chi = error.transpose() * error;
                     // if(chi > 1) cout << chi << endl;
-                    if(chi > 1) continue;
+                    // if(chi > 1) continue;
 
                     chi_tot += chi;
                     Matrix3f jac_t_i = (Matrix3f)(jacobians.block<3,3>(0, 0));
                     Matrix3f jac_t_j = (Matrix3f)(jacobians.block<3,3>(0, 3)); 
+                    Matrix3f jac_rot_i = (Matrix3f)(jacobians.block<3,3>(0, 6)); 
                     
                     int i = 3*cam.id;
                     int j = 3*cam2.id;
+                    int k = 3*( cam.id + cameras.size() );
+
                     matrix_H.block<3,3>(i, i) += jac_t_i.transpose()*jac_t_i;
                     matrix_H.block<3,3>(i, j) += jac_t_i.transpose()*jac_t_j;
+                    matrix_H.block<3,3>(i, k) += jac_t_i.transpose()*jac_rot_i;
+                    
                     matrix_H.block<3,3>(j, i) += jac_t_j.transpose()*jac_t_i;
                     matrix_H.block<3,3>(j, j) += jac_t_j.transpose()*jac_t_j;
+                    matrix_H.block<3,3>(j, k) += jac_t_j.transpose()*jac_rot_i;
+
+                    matrix_H.block<3,3>(k, i) += jac_rot_i.transpose()*jac_t_i;
+                    matrix_H.block<3,3>(k, j) += jac_rot_i.transpose()*jac_t_j;
+                    matrix_H.block<3,3>(k, k) += jac_rot_i.transpose()*jac_rot_i;
+
                     b.block<3,1>(i, 0) += jac_t_i.transpose()*error;
                     b.block<3,1>(j, 0) += jac_t_j.transpose()*error;
+                    b.block<3,1>(k, 0) += jac_rot_i.transpose()*error;
                 }
                 
             }
+
+            cout << "chi_tot error: " << chi_tot << endl;
+            if(chi_tot < 1) break;
             
             matrix_H += Eigen::MatrixXf::Identity(3*system_size, 3*system_size);
 
             Eigen::MatrixXf dx(3*system_size, 1);
             dx.setZero();
-            dx = matrix_H.fullPivLu().solve(b);
-            // dx = matrix_H.ldlt().solve(b);
+            dx = matrix_H.fullPivLu().solve(-b);
+            // dx = matrix_H.ldlt().solve(-b);
 
-            cout << "chi_tot error: " << chi_tot << endl;
-
-            // State perturbation
+            // State perturbation:
             for(int i=0; i < (int)cameras.size(); i++) {
-                Vec3f dx_i = dx.block<3,1>(3*i, 0);
-                cameras[i].position += dx_i;
+                Vec3f dx_i_position = dx.block<3,1>(3*i, 0);
+                cameras[i].position += dx_i_position;
+                Vec3f dx_i_orientation = dx.block<3,1>(3*(i + cameras.size()), 0);
+                // cout << i << " " << i + cameras.size()-1 << " " << cameras.size()-1 << " " << dx_i_orientation.transpose() << endl;
+                cameras[i].orientation += dx_i_orientation;
+                for(int j=0; j<3; j++) { // wrap each orientation angle to [-pi; pi)
+                    float wrapped_angle = cameras[i].orientation[j];
+                    wrapped_angle = (sin(wrapped_angle), cos(wrapped_angle));
+                    cameras[i].orientation[j] = wrapped_angle;
+                }
             }
-            for(int i=cameras.size(); i < system_size; i++) {
-                Vec3f dx_i = dx.block<3,1>(3*i, 0);
-                landmarks[landmark_index2id[i-cameras.size()]]+= dx_i;
+            for(int j=2*cameras.size(); j < system_size; j++) {
+                // cout << j-2*cameras.size() << "\t" << landmark_index2id[j-2*cameras.size()] << "\t" << landmark_id2index[landmark_index2id[j-2*cameras.size()]]<< endl;
+                Vec3f dx_j = dx.block<3,1>(3*j, 0);
+                landmarks[landmark_index2id[j-2*cameras.size()]] += dx_j;
             }
-
 
         }
-        
-
         
     }
 
--- src/bundle_adjustment.h
+++ src/bundle_adjustment.h
@@ -10,11 +10,11 @@
 
 namespace pr {
 
-    void error_and_jacobian_camera_poses(const Vec3f& t_ij, const Matrix3f& rot_i, const Vec3f& t_i, const Vec3f& t_j, Vec3f& error, Eigen::MatrixXf& jacobians);
+    void error_and_jacobian_camera_poses(const Vec3f& t_ij, const Vec3f& rot_angles_i, const Vec3f& t_i, const Vec3f& t_j, Vec3f& error, Eigen::MatrixXf& jacobians);
 
     void error_and_jacobian_position_landmark(const Camera& camera, const Vec3f& keypoint_dir_vector, const Vec3f& landmark, Vec3f& error, Eigen::MatrixXf& jacobians);
 
-    void bundle_adjustment(vector<Camera>& cameras, map<int, Vec3f>& landmarks);
+    void bundle_adjustment(vector<Camera>& cameras, map<int, Vec3f>& landmarks, int rounds);
 
 }
 
