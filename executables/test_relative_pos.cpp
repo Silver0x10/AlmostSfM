22d71f3eaa0720154208b61ad06431a0d3ff6375
Silver0x10 <smanganar@gmail.com>
2023-10-18 15:48:30 +0200

    start testing relative postion estimation function (8-point algorithm)

---
+++ executables/test_relative_pos.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <string>
+#include <vector>
+#include "utils.h"
+
+using namespace std;
+using namespace pr;
+
+int main (int argc, char** argv) {
+    // string dataset_path = "../dataset_and_info/input_BA.txt";
+    // vector<Camera> cameras = load_data(dataset_path);
+
+    cout << "Test rel pos" << endl;
+    
+    // bundle_adjustment(cameras, landmarks);
+
+    return 0;
+
+}
