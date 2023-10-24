94712f8dc7d64e8b881517122a772825fcfe01b4
Silver0x10 <smanganar@gmail.com>
2023-10-24 12:39:19 +0200

    icp3d fix with output test

--- src/icp_3d.cpp
+++ src/icp_3d.cpp
@@ -4,31 +4,27 @@ namespace pr {
 
     // Lesson 23b_registration_on_a_manifold taken as reference
 
-    void error_and_jacobian_icp_3d(Sim3 state, Vec3f landmark, Vec3f gt_landmark, Vec3f& error, Eigen::MatrixXf& jacobians) {
-        error = (state * landmark) - gt_landmark;
+    void error_and_jacobian_icp_3d(Sim3 state, Vec3f landmark, Vec3f gt_landmark, Vec3f& error, Eigen::MatrixXf& jacobian) {
+
+        Vec3f z_hat = state * gt_landmark;
+        error = z_hat - landmark;
 
         // wrt the translation components:
-        jacobians.block<3,3>(0, 0).setIdentity(); 
-
+        jacobian.block<3,3>(0, 0).setIdentity(); 
         // wrt the scaling component:
-        jacobians.block<3,1>(0, 3) = (state * landmark); 
-        // jacobians.block<3,1>(0, 3) = v2tRPY(state.rotation) * landmark; // wrt the scaling component
-
+        jacobian.block<3,1>(0, 3) = z_hat; 
         // wrt the camera orientation:
-        jacobians.block<3,3>(0, 4) = skew(-(state * landmark));
-        // jacobians.block<3,1>(0, 4) = Rx_prime(state.rotation[0])*Ry(state.rotation[1])*Rz(state.rotation[2]) * landmark; // wrt the camera orientation wrt x
-        // jacobians.block<3,1>(0, 5) = Rx(state.rotation[0])*Ry_prime(state.rotation[1])*Rz(state.rotation[2]) * landmark; // wrt the camera orientation wrt y
-        // jacobians.block<3,1>(0, 6) = Rx(state.rotation[0])*Ry(state.rotation[1])*Rz_prime(state.rotation[2]) * landmark; // wrt the camera orientation wrt z
+        jacobian.block<3,3>(0, 4) = skew(-z_hat);
     }
 
-    Sim3 icp_3d(map<int, Vec3f>& landmarks, map<int, Vec3f> gt_landmarks) {
+    Sim3 icp_3d(map<int, Vec3f>& landmarks, map<int, Vec3f> gt_landmarks, int iterations) {
         Sim3 state = Sim3();
 
         Eigen::MatrixXf matrix_H(7, 7);
         Eigen::MatrixXf b(7, 1);
 
-        for (int iteration = 0; iteration < 5; iteration++) {
-            cout << "ICP 3D iteration: " << iteration << endl;
+        for (int iteration = 0; iteration < iterations; iteration++) {
+            cout << "ICP 3D iteration: " << iteration << "\t->\t";
             matrix_H.setZero();
             b.setZero();
             float chi_tot = 0.0;
@@ -36,19 +32,27 @@ namespace pr {
             for(auto l: landmarks){
                 Vec3f error;
                 Eigen::MatrixXf jacobian(3, 7);
+                jacobian.setZero();
                 auto gt_l = gt_landmarks[l.first];
 
                 error_and_jacobian_icp_3d(state, l.second, gt_l, error, jacobian);
                 float chi = error.transpose() * error;
+
+                if(chi >= 1e6) {
+                    error *= sqrt(1e6/chi);
+                    chi = 1e6;
+                }
                 chi_tot += chi;
 
                 matrix_H += jacobian.transpose() * jacobian;
                 b += jacobian.transpose()*error;
             }
 
-            cout << "chi_tot error: " << chi_tot << endl;
-            if(chi_tot < 1) break;
+            cout << "Error: " << chi_tot << endl;
+            if(chi_tot < 0.01) break;
             
+            matrix_H += 0.6 * Eigen::MatrixXf::Identity(7, 7);
+
             Eigen::MatrixXf dx(7, 1);
             dx.setZero();
             dx = matrix_H.fullPivLu().solve(-b);
--- src/icp_3d.h
+++ src/icp_3d.h
@@ -6,8 +6,8 @@
 
 namespace pr {
 
-    void error_and_jacobian_icp_3d(Sim3 state, Vec3f landmark, Vec3f gt_landmark, Vec3f& error, Eigen::MatrixXf& jacobians);
+    void error_and_jacobian_icp_3d(Sim3 state, Vec3f landmark, Vec3f gt_landmark, Vec3f& error, Eigen::MatrixXf& jacobian);
 
-    Sim3 icp_3d(map<int, Vec3f>& landmarks, map<int, Vec3f> gt_landmarks);
+    Sim3 icp_3d(map<int, Vec3f>& landmarks, map<int, Vec3f> gt_landmarks, int iterations);
 
 }
