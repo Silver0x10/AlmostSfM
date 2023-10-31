0137041ca11f0a6228af487c6c78dc6ef81c5f5c
Silver0x10 <smanganar@gmail.com>
2023-10-31 12:19:01 +0100

    generalizing the usage of triangulation method to deal with the case when a landmark is seen from just one single camera

--- src/triangulation.cpp
+++ src/triangulation.cpp
@@ -4,31 +4,32 @@ namespace pr {
 
     // Reference: https://silo.tips/download/least-squares-intersection-of-lines
 
-    map<int, Vec3f> triangulate(const Camera& cam_i, const Camera& cam_j) {
-        map<int, Vec3f> cam_j_keypoints;
-        for(const Keypoint& kp: cam_j.keypoints) cam_j_keypoints.insert({kp.id, kp.direction_vector});
+    // map<int, Vec3f> triangulate(const Camera& cam_i, const Camera& cam_j) {
+    //     map<int, Vec3f> cam_j_keypoints;
+    //     for(const Keypoint& kp: cam_j.keypoints) cam_j_keypoints.insert({kp.id, kp.direction_vector});
 
-        map<int, Vec3f> landmarks;
-        for(const Keypoint& kp: cam_i.keypoints) {
-            if(cam_j_keypoints.find(kp.id) == cam_j_keypoints.end()) continue;
+    //     map<int, Vec3f> landmarks;
+    //     for(const Keypoint& kp: cam_i.keypoints) {
+    //         if(cam_j_keypoints.find(kp.id) == cam_j_keypoints.end()) continue;
             
-            Matrix3f matrix_H = Matrix3f::Identity() - kp.direction_vector*kp.direction_vector.transpose();
-            matrix_H += Matrix3f::Identity() - cam_j_keypoints[kp.id]*cam_j_keypoints[kp.id].transpose();
-            Vec3f b = ( Matrix3f::Identity() - kp.direction_vector*kp.direction_vector.transpose() ) * cam_i.position;
-            b += ( Matrix3f::Identity() - cam_j_keypoints[kp.id]*cam_j_keypoints[kp.id].transpose() ) * cam_j.position;
+    //         Matrix3f matrix_H = Matrix3f::Identity() - kp.direction_vector*kp.direction_vector.transpose();
+    //         matrix_H += Matrix3f::Identity() - cam_j_keypoints[kp.id]*cam_j_keypoints[kp.id].transpose();
+    //         Vec3f b = ( Matrix3f::Identity() - kp.direction_vector*kp.direction_vector.transpose() ) * cam_i.position;
+    //         b += ( Matrix3f::Identity() - cam_j_keypoints[kp.id]*cam_j_keypoints[kp.id].transpose() ) * cam_j.position;
 
-            Vec3f landmark_position = matrix_H.completeOrthogonalDecomposition().pseudoInverse() * b;
+    //         Vec3f landmark_position = matrix_H.completeOrthogonalDecomposition().pseudoInverse() * b;
 
-            landmarks.insert({kp.id, landmark_position});
-        }
+    //         landmarks.insert({kp.id, landmark_position});
+    //     }
 
-        return landmarks;
-    }
+    //     return landmarks;
+    // }
 
     map<int, Vec3f> triangulate(const vector<Camera>& cameras){
         map<int, Vec3f> landmarks;
         map<int, Matrix3f> matrices_H;
         map<int, Vec3f> vectors_b;
+        map<int, int> occurrences;
 
         for(const Camera& cam: cameras){
             for(const Keypoint& kp: cam.keypoints){
@@ -36,6 +37,7 @@ namespace pr {
                     Vec3f landmark_position;
                     landmark_position.setZero();
                     landmarks.insert({kp.id, landmark_position});
+                    occurrences.insert({kp.id, 1});
 
                     Matrix3f matrix_H = Eigen::Matrix3f::Identity() - kp.direction_vector*kp.direction_vector.transpose();
                     Vec3f b = ( Eigen::Matrix3f::Identity() - kp.direction_vector*kp.direction_vector.transpose() ) * cam.position;
@@ -45,10 +47,14 @@ namespace pr {
                 else {
                     matrices_H[kp.id] += Eigen::Matrix3f::Identity() - kp.direction_vector*kp.direction_vector.transpose();
                     vectors_b[kp.id] += ( Eigen::Matrix3f::Identity() - kp.direction_vector*kp.direction_vector.transpose() ) * cam.position;
+                    occurrences[kp.id] += 1;
                 }
             }
         }
 
+        for(auto l_occ: occurrences)
+            if(l_occ.second < 2) landmarks.erase(l_occ.first);
+        
         for(auto& l: landmarks) {
             Matrix3f matrix_H = matrices_H[l.first];
             Vec3f b = vectors_b[l.first];
--- src/triangulation.h
+++ src/triangulation.h
@@ -12,7 +12,7 @@ namespace pr {
 
     map<int, Vec3f> triangulate(const vector<Camera>& cameras);
 
-    map<int, Vec3f> triangulate(const Camera& camera0, const Camera& camera1);
+    // map<int, Vec3f> triangulate(const Camera& camera0, const Camera& camera1);
 
 }
 
