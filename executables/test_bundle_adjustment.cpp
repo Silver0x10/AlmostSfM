ec6f7bb0bd68f1c3498f46cdeee2dff9539edb35
Silver0x10 <smanganar@gmail.com>
2023-12-04 23:03:16 +0100

    cleaning test BA

--- executables/test_bundle_adjustment.cpp
+++ executables/test_bundle_adjustment.cpp
@@ -34,7 +34,7 @@ void visualize(const vector<Camera>& cameras, const map<int, pr::Vec3d>& landmar
     for(const auto& l: gt_landmarks) 
         gt_landmarks_cv.push_back( cv::Point3d(l.second.x(), l.second.y(), l.second.z()) );
     cv::viz::WCloud gt_landmarks_cloud(gt_landmarks_cv, cv::viz::Color::green());
-    gt_landmarks_cloud.setRenderingProperty( cv::viz::POINT_SIZE, 3 );
+    gt_landmarks_cloud.setRenderingProperty( cv::viz::POINT_SIZE, 5 );
     window.showWidget("gt_landmarks", gt_landmarks_cloud);
 
     // GT Cameras visualization (VIOLET)
@@ -51,30 +51,31 @@ void visualize(const vector<Camera>& cameras, const map<int, pr::Vec3d>& landmar
 int main (int argc, char** argv) {
     cout << "Test BundleAdjustment" << endl;
 
-    string dataset_path = "../../dataset_and_info/input_BA.txt";
-    // string dataset_path = argv[1];
+    string dataset_path = argv[1]; // "../../dataset_and_info/input_BA.txt"
     vector<Camera> cameras = load_data(dataset_path);
 
-    string gt_landmark_positions = "../../dataset_and_info/GT_landmarks.txt";
-    // string gt_landmark_positions = argv[2];
+    string gt_landmark_positions = argv[2]; // "../../dataset_and_info/GT_landmarks.txt"
     map<int, pr::Vec3d> gt_landmarks = load_landmarks(gt_landmark_positions);
 
     auto landmarks = triangulate(cameras);
     map<int, pr::Vec3d> landmarks_before_BA; for(auto l: landmarks) landmarks_before_BA.insert(l);
     
-    int ba_iterations = 5; //stoi(argv[1]);
+    int ba_iterations = 5;
     bool ba_linear_constraint = true;
     bundle_adjustment(cameras, landmarks, ba_iterations, ba_linear_constraint);
     
-    bool compare_to_GT = false;
+    bool compare_to_GT =  true;
     if (compare_to_GT) {
-        int icp_iterations = 5;
-        Sim3 estimated_transform = sicp_3d(landmarks, gt_landmarks, cameras[0].position, icp_iterations);
+        int icp_iterations = 300;
+        Sim3 initial_guess = Sim3();
+        initial_guess.translation = cameras[0].gt_position-cameras[0].position;
+        initial_guess.rotation = tRPY2v(v2tRPY(cameras[0].gt_orientation).transpose()*v2tRPY(cameras[0].orientation));
+        Sim3 estimated_transform = sicp_3d(landmarks, gt_landmarks, icp_iterations, initial_guess);
         cout << "\nSim3: \n" << estimated_transform.as_matrix() << endl; 
         for(auto& gt_l: gt_landmarks) gt_l.second = estimated_transform * gt_l.second;
         for(auto& cam: cameras) cam.gt_position = estimated_transform * cam.gt_position;
         visualize(cameras, landmarks, gt_landmarks);
-    } else { // compare wrt original estimated values
+    } else { // compare wrt original estimated values (to see how much positions have been changed)
         vector<Camera> cameras_before_BA = load_data(dataset_path);
         for(auto& cam: cameras) {
             cam.gt_position = cameras_before_BA[cam.id].position;
